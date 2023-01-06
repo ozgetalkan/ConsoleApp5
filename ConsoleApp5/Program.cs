c3c817275e5358b183962e31a436c15eff44fc0c
özge alkan <ozgetanulkualkan@gmail.com>
2023-01-06 19:54:50 +0300

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo di = new DirectoryInfo("c:\\");
+            var dirList = di.GetDirectories();
+            foreach (var folder in dirList)
+            {
+                Console.WriteLine(folder.Name);
+                Dön(folder);
+            }
+        }
+        static void Dön(DirectoryInfo di)
+        {
+            try
+            {
+                var dirList = di.GetDirectories();
+                foreach (var folder in dirList)
+                {
+                    Console.WriteLine(folder.Name);
+                    Dön(folder);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
